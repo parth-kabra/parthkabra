389514e4a888043c1c4d7fa2fe8f6daf609981cc
parth-kabra <kabraji001@gmail.com>
2022-10-28 18:39:29 +0530

    update files

---
+++ index.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, url_for
+
+app = Flask(__name__)
+
+@app.route("/", methods=["POST","GET"])
+@app.route("/home", methods=["POST","GET"])
+def home():
+    return render_template("index.html")
+
+@app.route('/freelance', methods=["POST", "GET"])
+def freelance():
+    return render_template("freelance.html")
+
+if __name__ == "__main__":
+    app.run(debug = True)
